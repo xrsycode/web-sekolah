642ff53a86bd161610f3e33f1317a754a20f4cbf
Osi ic <osiic.offcl@gmail.com>
2023-11-11 23:09:09 +0700

    heh

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@ let navbar = document.querySelector(".navbar");
 let searchBox = document.querySelector(".search-box .bx-search");
 // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-searchBox.addEventListener("click", ()=>{
+searchBox.addEventListener("click", () => {
   navbar.classList.toggle("showInput");
-  if(navbar.classList.contains("showInput")){
-    searchBox.classList.replace("bx-search" ,"bx-x");
-  }else {
-    searchBox.classList.replace("bx-x" ,"bx-search");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("bx-search", "bx-x");
+  } else {
+    searchBox.classList.replace("bx-x", "bx-search");
   }
 });
 
@@ -16,27 +16,24 @@ searchBox.addEventListener("click", ()=>{
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+let navLink = document.getElementsByClassName("navli");
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].children[1].onclick = () => {
+    navLink[i].children[2].classList.toggle("show1");
+  };
 }
 
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
- console.log(navLinks)
+let moreLinks = document.getElementsByClassName("more");
+for (let i = 0; i < moreLinks.length; i++) {
+  console.log(moreLinks[i]);
+  moreLinks[i].children[0].children[1].onclick = () => {
+    moreLinks[i].children[1].classList.toggle("show2");
+  };
 }
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
-}
-
-let navLink = document.querySelector(".links");
-console.log(navLink)
